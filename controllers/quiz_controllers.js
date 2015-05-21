fa3c93214705975c403fd836ef79d33e99a1b12e
jcpages <jc.pages@alumnos.upm.es>
2015-05-21 18:39:40 +0200

    Subir imagen

--- controllers/quiz_controllers.js
+++ controllers/quiz_controllers.js
@@ -70,6 +70,9 @@ exports.new = function(req, res) {
 // POST /quizes/create
 exports.create = function(req, res) {
 	req.body.quiz.UserId = req.session.user.id;
+	if(req.files.image){
+		req.body.quiz.imagen = req.files.image.name;
+	}
 	var quiz = models.Quiz.build( req.body.quiz );
     
     quiz
@@ -79,8 +82,9 @@ exports.create = function(req, res) {
     		res.render('quizes/new', {quiz: quiz, errors: err.errors});
     	} else {
 	// guardar en DB los campos pregunta y respuesta de quiz
-	quiz.save({fields: ["pregunta", "respuesta", "UserId"]}).then( function(){
-		res.redirect('/quizes')})
+	quiz
+	.save({fields: ["pregunta", "respuesta", "UserId", "image"]}).
+	then( function(){ res.redirect('/quizes')})
 	} // res.redirect: Redirección HTTP (URL relativo) lista de preguntas
    }
   ).catch(function(error){next(error)});
@@ -95,6 +99,9 @@ exports.edit = function(req, res) {
 
 // PUT /quizes/ :id 
 exports.update = function(req, res)  {
+	if(req.files.image){
+		req.quiz.image = req.files.image.name;
+	}
 	req.quiz.pregunta  = req.body.quiz.pregunta;
 	req.quiz.respuesta = req.body.quiz.respuesta;
 
@@ -106,11 +113,11 @@ exports.update = function(req, res)  {
 				res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
 			} else {
 				req.quiz  // save: guarda campos pregunta y respuesta en DB
-				.save( {fields: ["pregunta", "respuesta"]})
+				.save( {fields: ["pregunta", "respuesta", "image"]})
 				.then( function(){ res.redirect('/quizes');});
-			}
+			}    // Redirección HTTP a la lista de preguntas (URL relativo)
 		}
-	);
+	).catch(function(error){next(error)});
 };
 
 // DELETE /quizes/ :id 
--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -3,7 +3,7 @@ var models = require('../models/models.js');
 // MW que permite acciones solamente si el usuario objeto
 // pertenece con el usuario logeado o si es cuenta admin
 exports.ownershipRequired = function(req, res, next){
-    var objUser = req.quiz.id;
+    var objUser = req.user.id;
     var logUser = req.session.user.id;
     var isAdmin = req.session.user.isAdmin;
 
